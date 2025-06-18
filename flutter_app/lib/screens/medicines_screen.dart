9e269c0a2838ca15ffe51cc7b71deb1fbed3a3fc
Ahmadou0999 <133282263+Ahmadou0999@users.noreply.github.com>
2025-06-18 17:18:18 +0200

    Add flutter_app/lib/screens/medicines_screen.dart 1750259897247

---
+++ flutter_app/lib/screens/medicines_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+
+class MedicinesScreen extends StatefulWidget {
+  @override
+  _MedicinesScreenState createState() => _MedicinesScreenState();
+}
+
+class _MedicinesScreenState extends State<MedicinesScreen> {
+  final ApiService apiService = ApiService();
+  List<dynamic> medicines = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMedicines();
+  }
+
+  void fetchMedicines() async {
+    setState(() {
+      isLoading = true;
+    });
+    List<dynamic> fetchedMedicines = await apiService.fetchMedicines();
+    setState(() {
+      medicines = fetchedMedicines;
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Medicines'),
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: medicines.length,
+              itemBuilder: (context, index) {
+                final medicine = medicines[index];
+                return ListTile(
+                  title: Text(medicine['name']),
+                  subtitle: Text(medicine['description'] ?? ''),
+                  trailing: Text('\$${medicine['price']}'),
+                  onTap: () {
+                    // TODO: Implement medicine details or ordering
+                  },
+                );
+              },
+            ),
+    );
+  }
+}
